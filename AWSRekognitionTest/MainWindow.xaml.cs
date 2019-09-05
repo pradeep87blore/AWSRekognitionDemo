914422616eb4f82de35ec6d8ec37781ac1877ce8
Pradeep B <pradeep87blore@users.noreply.github.com>
2019-09-05 07:10:12 +0530

    First commit

---
+++ AWSRekognitionTest/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿//using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AWSRekognitionTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            // 
+        }
+
+        private void ButtonChooseImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            openFileDlg.Title = "Select Photo";
+
+            DialogResult dr = openFileDlg.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                Uri fileUri = new Uri(openFileDlg.FileName);
+                image_selectedImage.Source = new BitmapImage(fileUri);
+
+                Thread.Sleep(1000);
+                listBox_labels.Items.Clear();
+                var detectedLabels = RekognitionHelper.GetInfo(openFileDlg.FileName);
+
+                Console.WriteLine("Detected labels for " + openFileDlg.FileName);
+                foreach (var label in detectedLabels.Labels)
+                    listBox_labels.Items.Add(string.Format("{0}: {1}", label.Name, label.Confidence));
+            }
+
+            
+        }
+    }
+}
---
+++ AWSRekognitionTest/RekognitionHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.IO;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+
+namespace AWSRekognitionTest
+{
+    public class RekognitionHelper
+    {
+        public static DetectLabelsResponse GetInfo(string filePath)
+        {
+            String photo = filePath;
+
+            Amazon.Rekognition.Model.Image image = new Amazon.Rekognition.Model.Image();
+            try
+            {
+                using (FileStream fs = new FileStream(photo, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = null;
+                    data = new byte[fs.Length];
+                    fs.Read(data, 0, (int)fs.Length);
+                    image.Bytes = new MemoryStream(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to load file " + photo);
+                return null;
+            }
+
+            AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient();
+
+            DetectLabelsRequest detectlabelsRequest = new DetectLabelsRequest()
+            {
+                Image = image,
+                MaxLabels = 10,
+                MinConfidence = 77F
+            };
+
+            try
+            {
+                DetectLabelsResponse detectLabelsResponse = rekognitionClient.DetectLabels(detectlabelsRequest);
+                
+                Console.WriteLine("Detected labels for " + photo);
+                foreach (Label label in detectLabelsResponse.Labels)
+                    Console.WriteLine("{0}: {1}", label.Name, label.Confidence);
+
+                return detectLabelsResponse;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+    }
+}
